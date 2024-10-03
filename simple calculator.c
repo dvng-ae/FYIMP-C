971ba6418d9a3fcdc701145d152e4e6f7093da11
dvng-ae <devangrajeev2k2@gmail.com>
2024-10-03 11:47:18 +0530

    Create simple calculator.c

---
+++ simple calculator.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() 
+{
+    char operator;
+    double num1, num2, result;
+
+   
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+   
+    switch (operator) 
+    {
+        case '+':
+            result = num1 + num2;
+            printf("%.2f + %.2f = %.2f\n", num1, num2, result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("%.2f - %.2f = %.2f\n", num1, num2, result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("%.2f * %.2f = %.2f\n", num1, num2, result);
+            break;
+        case '/':
+            if (num2 != 0)
+            {
+                result = num1 / num2;
+                printf("%.2f / %.2f = %.2f\n", num1, num2, result);
+            } else
+            {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+        default:
+            printf("Error: Invalid operator.\n");
+            break;
+    }
+
+    return 0;
+}
