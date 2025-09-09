4c50710d5c28c69ca3939789e0a833572ad67056
dvng-ae <devangrajeev2k2@gmail.com>
2025-09-09 21:23:43 +0530

    linear search updated

--- linear-search.c
+++ linear-search.c
@@ -1,27 +1,31 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,search;
+    int n,i,search,found;
 
     printf("Enter the number of element :");
     scanf("%d",&n);
 
-    int arr[i];
+    int arr[n];
 
     printf("Enter the %d elements : ", n);
     for ( i = 0; i < n; i++)
     {
-        scanf("%d",arr[i]);
+        scanf("%d",&arr[i]);
     }
 
     printf("Enter the number to be searched : ");
-    scanf(|"%d",search);
+    scanf("%d",&search);
 
+    for ( i = 0; i < n; i++){
     if (arr[i]==search)
-    {
-        printf(("Element %d found at position %d\n",search,i+1);
+        {
+            printf("Element %d found at position %d\n",search,i+1);
+            found=1;
+            break;
+        }
     }
-    else
+    if (!found)
     {
         printf("Element %d not found in the array.\n",search);
     }
